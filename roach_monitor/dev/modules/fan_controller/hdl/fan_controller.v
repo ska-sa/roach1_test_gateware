9a15130d85a47af4ac31071959f3d24c70fac6d2
dave <dave@51f75b46-3331-0410-b433-c6c2fccb8461>
2008-02-15 14:03:56 +0000

    added skeleton fan controller
    git-svn-id: https://katfs.kat.ac.za/svnROACH/gw@532 51f75b46-3331-0410-b433-c6c2fccb8461

---
+++ roach_monitor/dev/modules/fan_controller/hdl/fan_controller.v
@@ -0,0 +1,41 @@
+`timescale 1ns/10ps
+
+  module fan_controller(
+    wb_clk_i, wb_rst_i,
+    wb_stb_i, wb_cyc_i, wb_we_i,
+    wb_adr_i, wb_dat_i, wb_dat_o,
+    wb_ack_o,
+    adc_result, adc_channel, adc_strb,
+    fan_sense, fan_control
+  );
+  parameter NUM_FANS = 3;
+  input  wb_clk_i, wb_rst_i;
+  input  wb_stb_i, wb_cyc_i, wb_we_i;
+  input  [15:0] wb_adr_i;
+  input  [15:0] wb_dat_i;
+  output [15:0] wb_dat_o;
+  output wb_ack_o;
+ 
+  input  adc_strb;
+  input   [4:0] adc_channel;
+  input  [11:0] adc_result;
+
+  input  [NUM_FANS - 1:0] fan_sense;
+  output [NUM_FANS - 1:0] fan_control;
+
+  assign wb_dat_o = 16'b0;
+  reg wb_ack_o;
+  assign fan_control = {NUM_FANS{1'b1}}; //on
+
+  always @(posedge wb_clk_i) begin
+    //strobes
+    wb_ack_o <= 1'b0;
+    if (wb_rst_i) begin
+    end else begin
+      if (wb_cyc_i & wb_stb_i & ~wb_ack_o) begin
+        wb_ack_o <= 1'b1;
+      end
+    end
+  end
+endmodule
+
---
+++ roach_monitor/dev/modules/fan_controller/tb/hdl/TB_fan_controller.v
@@ -0,0 +1,7 @@
+`timescale 1ns/1ps
+module TB_fan_controller();
+  initial begin
+    $display("PASSED");
+    $finish;
+  end
+endmodule
