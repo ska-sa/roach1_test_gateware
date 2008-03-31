eb8a2d00d741a5767263ac58885184980e18ed86
dave <dave@51f75b46-3331-0410-b433-c6c2fccb8461>
2008-03-31 11:15:07 +0000

    removed xaui_v7_1 'fixed' gen_build_parameters
    git-svn-id: https://katfs.kat.ac.za/svnROACH/gw@679 51f75b46-3331-0410-b433-c6c2fccb8461

--- roach_bsp/support/gen_build_parameters.v
+++ roach_bsp/support/gen_build_parameters.v
@@ -12,13 +12,13 @@
 `endif
 module gen_build_parameters();
   initial begin
-    $display("\`ifndef BUILD_PARAMETERS_VH");
-    $display("\`define BUILD_PARAMETERS_VH");
-    $display("\`define BOARD_ID  16'd`BOARD_ID");
-    $display("\`define REV_MAJOR 16'd`REV_MAJOR");
-    $display("\`define REV_MINOR 16'd`REV_MINOR");
-    $display("\`define REV_RCS   16'd`REV_RCS");
-    $display("\`endif");
+    $display("\140ifndef BUILD_PARAMETERS_VH");
+    $display("\140define BUILD_PARAMETERS_VH");
+    $display("\140define BOARD_ID  16'd`BOARD_ID");
+    $display("\140define REV_MAJOR 16'd`REV_MAJOR");
+    $display("\140define REV_MINOR 16'd`REV_MINOR");
+    $display("\140define REV_RCS   16'd`REV_RCS");
+    $display("\140endif");
     $finish;
   end
 endmodule
